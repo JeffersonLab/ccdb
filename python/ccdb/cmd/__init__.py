0eca27dd0c8078be2676235f51f0ada461e123c0
Dmitry Romanov <romanov@jlab.org>
2011-12-08 00:58:02 +0000

    Python library fix for ifarm
    git-svn-id: https://phys12svn.jlab.org/repos/trunk/ccdb@852 c5ed4466-e916-0410-8347-b3263e9c103d

--- python/ccdb/cmd/__init__.py
+++ python/ccdb/cmd/__init__.py
@@ -1,6 +1,6 @@
-from .ConsoleContext  import ConsoleContext
-from .ConsoleUtilBase import ConsoleUtilBase
-from .Theme import Theme
+from ConsoleContext  import ConsoleContext
+from ConsoleUtilBase import ConsoleUtilBase
+from Theme import Theme
 from colorama import Fore, Back, Style
 from Globals import get_verbose, set_verbose, is_debug_verbose, is_verbose, VerboseModes
 
