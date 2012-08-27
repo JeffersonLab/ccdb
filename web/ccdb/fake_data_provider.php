9413748d3a83e2ff342a11851b5e3bc35339bb76
Dmitry Romanov <romanov@jlab.org>
2012-08-27 23:20:31 +0000

    CCDB web interface update
    git-svn-id: https://phys12svn.jlab.org/repos/trunk/ccdb@1034 c5ed4466-e916-0410-8347-b3263e9c103d

--- web/ccdb/fake_data_provider.php
+++ web/ccdb/fake_data_provider.php
@@ -22,32 +22,78 @@ elseif($op==="objlist")
 {
 	echo '
 	[ 
-	   { "text": "1. Pre Lunch (120 min) <a href=\"http://google.com\">go</a> ", 
+	   { "text": "test", 
 		 "classes": "folder", 
 		 "children": 
 		    [ 
-			    { "text": "1.1 The State of the Powerdome (30 min)<a href=\"http://google.com\">go</a>" }, 
-				{ "text": "1.2 The Future of jQuery (30 min)" }, 
-				{ "text": "1.2 jQuery UI - A step to richnessy (60 min)" } 
+			    { "text": "test_vars" }, 
+				{ "text": "test_vars2" }, 
+				{ "text": "subfolder_for_tesing", "classes": "folder" } 
 			] 
 		}, 
-		{ "text": "2. Lunch (60 min)" }, 
-		{ "text": "3. After Lunch (120+ min)"} 
+		{ "text": "CDC", "classes": "folder", 
+		 "children": 
+		    [ 
+			    { "text": "test_vars" }, 
+				{ "text": "test_vars2" }, 
+				{ "text": "subfolder_for_tesing" } 
+			] },
+        { "text": "PMT", "classes": "folder" , 
+		 "children": 
+		    [ 
+			    { "text": "test_vars" }, 
+				{ "text": "test_vars2" }, 
+				{ "text": "subfolder_for_tesing" } 
+			] }, 
+		{ "text": "ADC", "classes": "folder", 
+		 "children": 
+		    [ 
+			    { "text": "test_vars" }, 
+				{ "text": "test_vars2" }, 
+				{ "text": "subfolder_for_tesing" } 
+			] },
+        { "text": "Magnet", "classes": "folder", 
+		 "children": 
+		    [ 
+			    { "text": "test_vars" }, 
+				{ "text": "test_vars2" }, 
+				{ "text": "subfolder_for_tesing" } 
+			] } 
 	]';
 }
 elseif($op==="data")
 {
-	echo '{ "aaData": [
-	["0", "2147483647", "default", "10/31/2009 15:08", "2009-10-31 15:08"],
-  	["0", "2147483647", "default", "10/28/2011 14:11", "2011-10-28 14:11"],
-  	["1", "1000"      , "default", "10/27/2011 15:08", "2011-10-27 15:08"],
-  	["0", "2147483647", "dmitry" , "09/14/2011 16:19", "2011-09-14 16:19"]
-	],
-	"aaColumns": ["opa", "apa", "tet","sum", "soho"	],
-	"aaComments":"This is a test table we should be glad we have it!"
-	
-	}';
-	//
+
+echo '{ "aaData": [
+["1.12", "5.987", "0.99245", "10.22", "coil 1"],
+["1.13", "5.987", "0.1235234", "12.12", "coil 2"],
+["1.11", "6.988", "0.123236", "0", "coil 3"],
+["2.23", "5.957", "0.99276", "0", "coil 4"],
+["1.53", "5.981", "0.929486", "0", "coil 5"],
+["1.55", "5.987", "0.23486", "1.12", "coil 6"],
+["1.10", "5.987", "0.2343", "5.34e-10", "coil 7"],
+["0.21", "5.987", "0.22423", "0.115", "coil 8"],
+["0.89", "5.987", "0.296884", "1923.12", "coil 9"],
+["1.11", "5.987", "0.230967", "1.00123", "coil 10"]
+
+],
+"aaColumns": ["c0", "c1", "c2","c3", "description"	],
+"aaComments":"This is a test table. It has 5 columns and 10 rows with. 4 columns contains double values, last column is string containing description. "
+
+}';
+
+/*
+echo '{ "aaData": [
+["0", "2147483647", "default", "10/31/2009 15:08", "2009-10-31 15:08"],
+["0", "2147483647", "default", "10/28/2011 14:11", "2011-10-28 14:11"],
+["1", "1000"      , "default", "10/27/2011 15:08", "2011-10-27 15:08"],
+["0", "2147483647", "dmitry" , "09/14/2011 16:19", "2011-09-14 16:19"]
+],
+"aaColumns": ["opa", "apa", "tet","sum", "soho"	],
+"aaComments":"This is a test table we should be glad we have it!"
+
+}';
+*/	//
 	
 }
 ?>
