c4962de5b37b7ac31a9c23b80616abec892f4d2c
Dmitry Romanov <romanov@jlab.org>
2012-12-11 17:27:03 +0000

    Development of python API for web interface
    git-svn-id: https://phys12svn.jlab.org/repos/trunk/ccdb@1148 c5ed4466-e916-0410-8347-b3263e9c103d

---
+++ web/cgi-bin/ccdb_web_config.py
@@ -0,0 +1,43 @@
+#config file for ccdb cgi scripts
+
+
+#database connection string. connection string might be in form:
+# mysql://<username>:<password>@<mysql.address>:<port> <database>
+# sqlite:///path/to/file.sqlite
+connection_string = "mysql://ccdb_user@127.0.0.1/ccdb"
+
+
+# if ccdb/python directory is not in PYTHONPATH this setting will force
+# to add ccdb_python_path to python modules search path when python_config() is called
+force_import_ccdb_python = True
+
+
+# Path of the ccdb/python directory.
+# This setting is relevant only if force_import_ccdb_python = True
+ccdb_python_path = "/home/romanov/halld/ccdb/trunk/python"
+
+
+# cgitb is a python module which is capable of displaying python errors to browser
+cgitb_is_enabled = True
+
+#-------------- end of configuration file -------------------------
+
+
+def python_config():
+    """
+    setup python environment for running website
+
+    :return: None
+    """
+
+    # force to add ccdb_python_path
+    if force_import_ccdb_python:
+        import sys
+        sys.path.append(ccdb_python_path)
+
+    #displaying python errors to browser
+    if cgitb_is_enabled:
+        import cgitb
+        cgitb.enable()
+
+
