0f18d6b5c6eda4a2c35abd1193450d23ac70e172
Dmitry Romanov <romanov@jlab.org>
2013-01-03 09:29:57 +0000

    Cleanup of old projects
    git-svn-id: https://phys12svn.jlab.org/repos/trunk/ccdb@1159 c5ed4466-e916-0410-8347-b3263e9c103d

--- projects/CodeLite/ccdb_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ Tests - Debug ]----------
-	@"$(MAKE)" -f "Tests.mk"
-clean:
-	@echo ----------Cleaning project:[ Tests - Debug ]----------
-	@"$(MAKE)" -f "Tests.mk" clean
