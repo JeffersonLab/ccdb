93fa82ec6454126e095f8f5c6120929ba27034a4
Mark Ito <gluex@jlab.org>
2014-01-17 13:56:23 +0000

    Fixed bug in my original script. Insert missing comma between group lines. M    group_parse.pl
    git-svn-id: https://phys12svn.jlab.org/repos/trunk/ccdb@1504 c5ed4466-e916-0410-8347-b3263e9c103d

--- scripts/users_create/group_parse.pl
+++ scripts/users_create/group_parse.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/env perl
 $groupid = 267;
 open (YPCAT,"ypcat group |");
+$group_count = 0;
 while (<YPCAT>) {
     if (/:$groupid:/) {
+	$group_count++;
+	if ($group_count > 1) {print ',';}
 	chomp;
 	@t = split/:$groupid:/;
 	print $t[1];
