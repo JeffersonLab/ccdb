f853fad082e8f50707b75b8dcaf5ff6ac108b629
Dmitry Romanov <romanov@jlab.org>
2014-04-11 18:48:40 +0000

    Infer status by file
    git-svn-id: https://phys12svn.jlab.org/repos/trunk/ccdb@1803 c5ed4466-e916-0410-8347-b3263e9c103d

--- python/ccdb/table_file.py
+++ python/ccdb/table_file.py
@@ -2,6 +2,13 @@ import shlex
 
 __all__ = ["TextFileDOM"]
 
+META_FULL_REQUEST = 'full request'
+META_VARIATION = 'variation'
+META_CREATED = 'created'
+META_RUN_RANGE = 'run range'
+META_AUTHOR = 'author'
+
+
 #*********************************************************************
 #   Class TextFileDOM - store information of text data files         *
 #                                                                    *
@@ -11,29 +18,28 @@ class TextFileDOM(object):
 
     def __init__(self):
         self.comment_lines = []
-        self.metas={}
-        self.rows=[] #each row should consist of [] - column item
-        self.column_names=[]
-        self.inconsistent_reason=""
-    
-    @property
-    def data_is_consistant(self):
-        #TODO refactor typo
+        self.metas = {}
+        self.rows = []  # each row should consist of [] - column item
+        self.column_names = []
         self.inconsistent_reason = ""
-        if not self.has_data: 
+
+    @property
+    def data_is_consistent(self):
+        self.inconsistent_reason = ""
+        if not self.has_data:
             self.inconsistent_reason = "File has no data"
             return False
 
         for row_index, row in enumerate(self.rows):
             if len(row) != self.columns_length:
                 self.inconsistent_reason = "Row length mismatch. " \
-                                           "Row '{0}' has '{1}' values while awaited number of columns is '{2}'".\
-                                           format(row_index, len(row), self.columns_length)
+                                           "Row '{0}' has '{1}' values while awaited number of columns is '{2}'". \
+                    format(row_index, len(row), self.columns_length)
                 return False
 
         if len(self.column_names) != self.columns_length:
-            self.inconsistent_reason = "Column names number is: '{0}'. It is not equal to data columns number: '{1}'".\
-                                       format(len(self.column_names), self.columns_length)
+            self.inconsistent_reason = "Column names number is: '{0}'. It is not equal to data columns number: '{1}'". \
+                format(len(self.column_names), self.columns_length)
             return True
         #if we are here, everything is good
         return True
@@ -50,7 +56,7 @@ class TextFileDOM(object):
         #now we know there is at least 1 row
         return len(self.rows[0])
 
-    
+
     @property
     def has_data(self):
         if not len(self.rows):
@@ -62,56 +68,56 @@ class TextFileDOM(object):
         except IndexError:
             pass
         return False
-        
+
 
 #----------------------------------------
 #   read_ccdb_text_file 
 #---------------------------------------- 
 def read_ccdb_text_file(file_name):
-    dom = TextFileDOM()    
+    dom = TextFileDOM()
     try:
         with open(file_name) as f:
             for line in f:
                 assert isinstance(line, str)
-                
+
                 #prepare line and skip empty one
                 line = line.strip()
-                if not len(line): continue
-                
-                
+                if not len(line):
+                    continue
+
                 #what is this line?
-                if line.startswith('#meta'):   #comment with meta
+                if line.startswith('#meta'):  # comment with meta
                     line = line[5:].strip()
-                    if (":" in line) and (line.index(":") != len(line)-1):
+                    if (":" in line) and (line.index(":") != len(line) - 1):
                         key = line[:line.index(":")].strip()
-                        val = line[line.index(":")+1:].strip()
-                        dom.metas[key]=val
+                        val = line[line.index(":") + 1:].strip()
+                        dom.metas[key] = val
                     else:
-                        dom.metas[line]=""
-                        
-                elif line.startswith('#&'):         # comment with column names
+                        dom.metas[line] = ""
+
+                elif line.startswith('#&'):  # comment with column names
                     line = line[2:].strip()
                     dom.column_names = shlex.split(line)
-                        
-                elif line.startswith("#"):          # comment
+
+                elif line.startswith("#"):  # comment
                     line = line[1:]
                     dom.comment_lines.append(line)
-                    
-                else:                               # string with data?
+
+                else:  # string with data?
                     tokens = shlex.split(line)
                     values = []
                     for token in tokens:
-                        if token.startswith("#"):   # stop loop if the comment is met
-                            break 
+                        if token.startswith("#"):  # stop loop if the comment is met
+                            break
                         else:
                             values.append(token)
-                            
+
                     dom.rows.append(values)
-                    
+
     except IOError as error:
         #error open or read file...
         raise error
-    
+
     #everything is fine?
     return dom
 
@@ -119,45 +125,46 @@ def read_ccdb_text_file(file_name):
 #----------------------------------------
 #   read_namevalue_text_file 
 #---------------------------------------- 
-def read_namevalue_text_file(file_name, replace_c_comments = False):
+def read_namevalue_text_file(file_name, replace_c_comments=False):
     """
     :param replace_c_comments: - if file contains // - 'C' style comments, replace them by # first
     :type replace_c_comments: bool
     """
-    dom = TextFileDOM()    
+    dom = TextFileDOM()
     try:
         with open(file_name) as f:
             values = []
             for line in f:
                 assert isinstance(line, str)
-                
+
                 #prepare line and skip empty one
                 line = line.strip()
-                if replace_c_comments: line = line.replace("//","#")
-                if not len(line): continue
-                
+                if replace_c_comments:
+                    line = line.replace("//", "#")
+                if not line:
+                    continue
+
                 #what is this line?
-                if line.startswith("#"):     #comment
+                if line.startswith("#"):    # comment
                     line = line[1:]
                     dom.comment_lines.append(line)
-                    
-                else:                          #string with data?
-                    tokens = []
+
+                else:   # string with data?
                     tokens = shlex.split(line)
-                    
+
                     #check we have name and value
-                    if len(tokens) < 2: 
+                    if len(tokens) < 2:
                         print "ERROR. The name-value file have less than 2 columns. So where are names and values?"
                         raise IOError()
                     values.append(tokens[1])
                     dom.column_names.append(tokens[0])
-           
+
             #add line       
             dom.rows.append(values)
-                    
+
     except IOError as error:
         #error open or read file...
         raise error
-    
+
     #everything is fine?
     return dom                   
