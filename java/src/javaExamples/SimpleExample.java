b35c61c4fd9cbf6bad900b56bfdeedd574887d07
Dmitry Romanov <romanov@jlab.org>
2014-04-15 23:57:25 +0000

    Java version code gardening
    git-svn-id: https://phys12svn.jlab.org/repos/trunk/ccdb@1817 c5ed4466-e916-0410-8347-b3263e9c103d

--- java/src/javaExamples/SimpleExample.java
+++ java/src/javaExamples/SimpleExample.java
@@ -8,5 +8,6 @@ public class SimpleExample {
         provider.connect();
         Directory dir = provider.getDirectory("/test");
         System.out.println("Hello World! " + dir.getId());
+
     }
 }
--- java/src/org/ccdb/PathUtils.kt
+++ java/src/org/ccdb/PathUtils.kt
@@ -4,6 +4,10 @@
 
 package org.ccdb.helpers
 
+import java.util.Date
+import java.text.SimpleDateFormat
+import org.omg.CORBA.Request
+
 /** @brief Extracts Directory from object path
  *
  *	in /dir/subdir/object will return /dir/subdir
@@ -11,9 +15,9 @@ package org.ccdb.helpers
  * @param    const string & path
  * @return   std::string
  */
-fun extractDirectory(path:String): String{
+fun extractDirectory(path: String): String {
     val index = path.lastIndexOf("/")
-    if(index <= 0) return ""
+    if (index <= 0) return ""
     return path.substring(0, index)
 }
 
@@ -22,9 +26,9 @@ fun extractDirectory(path:String): String{
  * @param  const string & path
  * @return std::string
  */
-fun extractObjectname(path:String ):String{
+fun extractObjectname(path: String): String {
     val index = path.lastIndexOf("/")
-    if(index < 0 || index == path.size - 1) return ""
+    if (index < 0 || index == path.size - 1) return ""
     return path.substring(index + 1)
 }
 
@@ -37,26 +41,22 @@ fun extractObjectname(path:String ):String{
  * @param     const string & right
  * @return   std::string
  */
-fun combinePath( left:String, right:String ):String
-{
-    if(right.length == 0) return left
-    if(left.length == 0)  return right
+fun combinePath(left: String, right: String): String {
+    if (right.length == 0) return left
+    if (left.length == 0) return right
     val separator = '/'
     var result = left;
     var needAddSeparator = false;
-    var leftLast = left[left.length-1];
+    var leftLast = left[left.length - 1];
     var rightFirst = right[0];
 
-    if((leftLast == separator) && (rightFirst == separator))
-    {
+    if ((leftLast == separator) && (rightFirst == separator)) {
         //it is a situation we have both "left/ + /right"
         //so erase one of them
         result = left.substring(0, left.length)
 
         //needAddSeparator should be false by default so we dont touch it
-    }
-    else if((leftLast != separator) && (rightFirst != separator))
-    {
+    } else if ((leftLast != separator) && (rightFirst != separator)) {
         //it is a situation we have "left + right"
         //needs separator
         needAddSeparator = true;
@@ -65,11 +65,11 @@ fun combinePath( left:String, right:String ):String
     //The last case (leftLast != separator) || (rightFirst != separator)
     //gives us needAddSeparator = false, but it is false by default
 
-    if(needAddSeparator) result += separator;
-    return result+right;
+    if (needAddSeparator) result += separator;
+    return result + right;
 }
 
-//
+
 ////______________________________________________________________________________
 //bool ccdb::PathUtils::WildCardCheck( const char* pattern, const char* source )
 //{
@@ -109,157 +109,154 @@ fun combinePath( left:String, right:String ):String
 //return !*pattern;
 //}
 
-//
-////______________________________________________________________________________
-//time_t ccdb::PathUtils::ParseTime( const string &timeStr, bool * succsess )
-//{
-///** @brief ParseTime
-// * parses time as any part of
-// * YYYY:MM:DD-hh:mm:ss
-// *
-// * 1) in place of ':' and '-' might be any ONE non digit character
-// *
-// * 2) One could place only part of the string,
-// *    the latest date for this part will be returned.
-// *    I.e. if timeString is "2011" - (it means the year 2011 and nothing more),
-// *    this function returns result as if it were 2011:12:31-23:59:59
-// *    thus using such timestamp would get the latest constants for year 2011
-// *
-// * @parameter [in] const string & timeString string to parse
-// * @parameter [out] succsess true if success
-// * @return   time_t
-// */
-//
-////default result
-//tm time;
-//time.tm_hour = 23;
-//time.tm_mday = 31;
-//time.tm_min = 59;
-//time.tm_mon = 11;
-//time.tm_sec = 59;
-//time.tm_isdst=false;
-//if(succsess!=NULL) *succsess = true;
-//
-////some tmp values
-//string tmpStr =""; //tmp string to buffer chars
-//int delimCount=0;  //number of delimeters we've met
-//
-//string workStr(timeStr); //we appended a work string for one more non digit symbol
-//workStr.push_back(' ');  //so the logic behind would work in any case
-//
-//bool lastIsDigit;   //last symbol was digit
-//
-////scan all symbols
-//for (size_t i=0; i<workStr.size(); i++)
-//{
-//char symbol = workStr[i];
-//
-//if(symbol>='0'&&symbol<='9') //Check if it is number
-//{
-//tmpStr.push_back(symbol);
-//lastIsDigit = true;
-//}
-//else if(lastIsDigit) //it is a delimeter after the value;
-//{
-//delimCount++;
-//if(delimCount ==1) //it was a year
-//{
-//if(tmpStr.length()!=4) //it is an error with lengtn of year
-//{
-//if(succsess!=NULL) *succsess = false;
-//return 0;
-//}
-//time.tm_year = StringUtils::ParseInt(tmpStr) - 1900;  //since in tm the year is from 1900
-//}
-//
-//if(delimCount ==2) //it was a month
-//{
-//if(tmpStr.length()!=2) //it is an error with lengtn of year
-//{
-//if(succsess!=NULL) *succsess = false;
-//return 0;
-//}
-//
-//time.tm_mon = StringUtils::ParseInt(tmpStr) - 1;  // -1 becaouse months since January [0-11]
-//
-////#check for 30 day month
-//if((time.tm_mon == 9)||(time.tm_mon == 10)||(time.tm_mon == 4)||(time.tm_mon == 6))
-//{
-//time.tm_mday = 30;
-//}
-//
-////February...
-//if(time.tm_mon == 2)
-//{
-//if( (time.tm_year % 4 == 0 &&  time.tm_year % 100 != 0) || time.tm_year % 400 == 0)
-//{
-//time.tm_mday = 29;
-//}
-//else
-//{
-//time.tm_mday = 28;
-//}
-//}
-//}
-//
-//if(delimCount ==3) //it was a day
-//{
-//if(tmpStr.length()!=2) //it is an error with lengtn of day
-//{
-//if(succsess!=NULL) *succsess = false;
-//return 0;
-//}
-//time.tm_mday = StringUtils::ParseInt(tmpStr);  //since in tm the year is from 1900
-//}
-//
-//if(delimCount ==4) //it was a hour
-//{
-//if(tmpStr.length()!=2) //it is an error with lengtn
-//{
-//if(succsess!=NULL) *succsess = false;
-//return 0;
-//}
-//time.tm_hour = StringUtils::ParseInt(tmpStr);  //since in tm the year is from 1900
-//}
-//
-//if(delimCount ==5) //it was a min
-//{
-//if(tmpStr.length()!=2) //it is an error with lengtn of min
-//{
-//if(succsess!=NULL) *succsess = false;
-//return 0;
-//}
-//time.tm_min = StringUtils::ParseInt(tmpStr);  //since in tm the year is from 1900
-//}
-//
-//if(delimCount ==6) //it was a sec
-//{
-//if(tmpStr.length()!=2) //it is an error with lengtn of sec
-//{
-//if(succsess!=NULL) *succsess = false;
-//return 0;
-//}
-//time.tm_sec = StringUtils::ParseInt(tmpStr);  //since in tm the year is from 1900
-//}
-//
-//
-////clear temp string for the next digit seria
-//tmpStr.clear();
-//}
-//else
-//{
-//tmpStr.clear(); //in this case we clear temp string too
-//}
-//}
-//time_t result = mktime(&time);
-//if( result == -1 && succsess!=NULL) *succsess = false;
-//return result;
-//}
-//
-//
-////______________________________________________________________________________
-//ccdb::RequestParseResult ccdb::PathUtils::ParseRequest( const string& requestStr )
-//{
+
+/** @brief ParseTime
+ * parses time as any part of
+ * YYYY:MM:DD-hh:mm:ss
+ *
+ * 1) in place of ':' and '-' might be any ONE non digit character
+ *
+ * 2) One could place only part of the string,
+ *    the latest date for this part will be returned.
+ *    I.e. if timeString is "2011" - (it means the year 2011 and nothing more),
+ *    this function returns result as if it were 2011:12:31-23:59:59
+ *    thus using such timestamp would get the latest constants for year 2011
+ *
+ * @parameter [in] const string & timeString string to parse
+ * @return   parsed time
+ */
+fun parseTime(timeStr: String): Date {
+    //default result
+    var year = 3000
+    var mon = 11
+    var hour = 23
+    var day = 31
+    var min = 59
+    var sec = 59
+
+    //some tmp values
+    var tmpStr = ""              // tmp string to buffer chars
+    var delimCount = 0;            // number of delimeters we've met
+    var workStr = timeStr + " "  // we appended a work string for one more non digit symbol
+    var lastIsDigit = false      // last symbol was digit
+
+    //scan all symbols
+    for (i in 0..(workStr.size - 1)) {
+        val symbol = workStr[i]
+
+
+        if (symbol.isDigit()) {
+            // Check if it is number
+            tmpStr += symbol
+            lastIsDigit = true
+        } else {
+            if (lastIsDigit) {
+                // It is a delimiter after the value;
+                delimCount++
+                if (delimCount == 1) //it was a year
+                {
+                    if (tmpStr.size != 4) //it is an error with length
+                    {
+                        throw IllegalArgumentException("Year should be in form YYYY but given is '$tmpStr'. Overall date should be in form 'YYYY/MM/dd hh:mm:ss'")
+                    }
+                    year = Integer.parseInt(tmpStr)   // since in tm the year is from 1900
+                }
+
+
+                if (delimCount == 2) //it was a month
+                {
+                    if (tmpStr.size != 2) //it is an error with length
+                    {
+                        throw IllegalArgumentException("Month should be in form 'MM' but given is '$tmpStr'. Overall date should be in form 'YYYY/MM/dd hh:mm:ss'")
+                    }
+
+
+                    mon = Integer.parseInt(tmpStr) - 1  // -1 because months since January [0-11]
+
+                    //check for 30 day month
+                    if ((mon == 9) || (mon == 10) || (mon == 4) || (mon == 6)) {
+                        day = 30
+                    }
+
+                    //February...
+                    if (mon == 2) {
+                        if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                            day = 29
+                        } else {
+                            day = 28
+                        }
+                    }
+                }
+
+                if (delimCount == 3) //it was a day
+                {
+
+                    if (tmpStr.size != 2) //it is an error with length
+                    {
+                        throw IllegalArgumentException("Day should be in form 'dd' but given is '$tmpStr'. Overall date should be in form 'YYYY/MM/dd hh:mm:ss'")
+                    }
+                    day = Integer.parseInt(tmpStr);  //since in tm the year is from 1900
+                }
+
+                if (delimCount == 4) //it was a hour
+                {
+                    if (tmpStr.size != 2) //it is an error with length
+                    {
+                        throw IllegalArgumentException("Hour should be in form 'hh' but given is '$tmpStr'." +
+                        "Overall date should be in form 'YYYY/MM/dd hh:mm:ss'")
+                    }
+
+                    hour = Integer.parseInt(tmpStr)
+                }
+
+                if (delimCount == 5) //it is a minutes
+                {
+                    if (tmpStr.size != 2) //it is an error with length
+                    {
+                        throw IllegalArgumentException("Minutes should be in form 'mm' but given is '$tmpStr'." +
+                        "Overall date should be in form 'YYYY/MM/dd hh:mm:ss'")
+                    }
+
+                    min = Integer.parseInt(tmpStr)
+                }
+
+                if (delimCount == 6) //it is seconds
+                {
+                    if (tmpStr.size != 2) //it is an error with length
+                    {
+                        throw IllegalArgumentException("Seconds should be in form 'ss' but given is '$tmpStr'." +
+                        "Overall date should be in form 'YYYY/MM/dd hh:mm:ss'")
+                    }
+
+                    sec = Integer.parseInt(tmpStr)
+                }
+            }
+            tmpStr = ""     // Clear the temp string
+            lastIsDigit = false
+        }
+    }
+
+    return Date(year, mon, day, hour, min, sec)
+}
+
+//Set default parameters
+// result
+class RequestParseResult {
+    var runNumber=0;	           // Run number
+    var wasParsedRunNumber=false   // true if Run number was non empty
+    var isInvalidRunNumber=false   // true if was an error parsing runnumber
+    var path = "";                 // Object path
+    var wasParsedPath=false        // true if Path was nonempty
+    var variation=""               // Variation name
+    var wasParsedVariation=false   // true if variation was not empty
+    var time=Date()                // Time stamp
+    var wasParsedTime=false        // true if time stamp was not empty
+    var timeString=""              // Original string with time
+}
+
+
+
+fun parseRequest( requestStr:String ): RequestParseResult{
 ///** @brief    Parses request string and returns corresponding
 // * @see DParseRequestResult structure.
 // *
@@ -275,73 +272,57 @@ fun combinePath( left:String, right:String ):String
 // * @return structure that represent user result
 // */
 //
-////Set default parameters
-//RequestParseResult result;
-//result.RunNumber=0;	              // Run number
-//result.WasParsedRunNumber=false;  // true if Run number was non empty
-//result.IsInvalidRunNumber=false;  // true if was an error parsing runnumber
-//result.Path = "";                 // Object path
-//result.WasParsedPath=false;       // true if Path was nonempty
-//result.Variation="";              // Variation name
-//result.WasParsedVariation=false;  // true if variation was not empty
-//result.Time=0;                    // Time stamp
-//result.WasParsedTime=false;       // true if time stamp was not empty
-//result.TimeString="";             // Original string with time
-//
-//int colonCount=0;
-//string runStr ="";
-//for (size_t i=0; i<requestStr.size(); i++)
-//{
-//char symbol = requestStr[i];
-//if(symbol!=':')
-//{
-////it is not a colon so we add this symbol somewhere
-//switch(colonCount)
-//{
-//case 0:  //it is a path
-//result.Path.push_back(symbol);
-//result.WasParsedPath=true;
-//break;
-//case 1:  //it is a run range
-//runStr.push_back(symbol);
-//result.WasParsedRunNumber = true;
-//break;
-//case 2:  //it is variation
-//result.Variation.push_back(symbol);     // Variation name
-//result.WasParsedVariation=true;         // true since we've got a variation
-//break;
-//default:  //it should be a time than
-//result.WasParsedTime=true;              // true if time stampt was not empty
-//result.TimeString.push_back(symbol);    // Original string with time
-//break;
-//}
-//}
-//else //the symbol is colon (symbol==':')
-//{
-//colonCount++;
-//}
-//}
-//
-//// at this point we parsed all symbols and it is time for final parsing
-//
-////parse run number
-//if(result.WasParsedRunNumber)
-//{
-//bool success = true;
-//result.RunNumber = StringUtils::ParseInt(runStr, &success);
-//
-//result.IsInvalidRunNumber = !success || result.RunNumber<0;
-//}
-//
-////parse time
-//if(result.WasParsedTime)
-//{
-//bool success=true;
-//result.Time = ParseTime(result.TimeString, &success);
-//}
-//return result;
-//}
-//
+    val result = RequestParseResult()
+    var colonCount = 0
+    var runStr = ""
+    for (i in 0..(requestStr.size-1)) {
+        val symbol = requestStr[i]
+        if (symbol != ':') {
+            //it is not a colon so we add this symbol somewhere
+            when(colonCount) {
+                0 -> {
+                    //it is a path
+                    result.path += symbol
+                    result.wasParsedPath = true;
+                }
+                1 -> {
+                    //it is a run range
+                    runStr += symbol
+                    result.wasParsedRunNumber = true;
+                }
+                2 -> {
+                    //it is variation
+                    result.variation += symbol          // Variation name
+                    result.wasParsedVariation = true;   // true since we've got a variation
+                }
+                else -> {
+                    //it should be a time than
+                    result.timeString += symbol    // Original string with time
+                    result.wasParsedTime = true;   // true if time stamp was not empty
+                }
+            }
+        } else{
+            //the symbol is colon (symbol==':')
+            colonCount++;
+
+            //This is patch for situation when time string has ':' inside
+            if(colonCount>3) result.timeString += ':'
+        }
+    }
+
+    //at this point we parsed all symbols and it is time for final parsing
+    //parse run number
+    if(result.wasParsedRunNumber){
+        result.runNumber = Integer.parseInt(runStr)
+    }
+
+    //parse time
+    if(result.wasParsedTime){
+        result.time = parseTime(result.timeString)
+    }
+
+    return result
+}
 
 /** @brief Adds '/' to the beginning of the path if it is not there
  *
@@ -350,16 +331,14 @@ fun combinePath( left:String, right:String ):String
  * @parameter [in] string & path
  * @return   void
  */
-fun makeAbsolute( path:String ):String
-{
-    if( !isAbsolute(path) ) return '/' + path;
+fun makeAbsolute(path: String): String {
+    if ( !isAbsolute(path) ) return '/' + path;
     return path;
 }
 
 /** @brief Check if the path is absolute - starts with /
  */
-fun isAbsolute(path:String):Boolean
-{
+fun isAbsolute(path: String): Boolean {
     return (path.length > 0) && (path[0] == '/')
 }
 
