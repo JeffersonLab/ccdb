febbddab7e0c9aef2ddd9e04c3b38f93788cb1bc
Dmitry Romanov <romanov@jlab.org>
2014-04-21 21:15:10 +0000

    Java example update
    git-svn-id: https://phys12svn.jlab.org/repos/trunk/ccdb@1854 c5ed4466-e916-0410-8347-b3263e9c103d

--- java/src/javaExamples/SimpleExample.java
+++ java/src/javaExamples/SimpleExample.java
@@ -6,12 +6,19 @@ import java.util.Date;
 public class SimpleExample {
 
     public static void main(String [ ] args){
-//        MySqlProvider provider = new MySqlProvider("mysql://localhost");
         JDBCProvider provider = CcdbPackage.createProvider("mysql://localhost")  ;
         provider.connect();
         Assignment asgmt = provider.getData("/test/test_vars/test_table");
-//
+
         System.out.println(asgmt.getBlob());
         System.out.println(asgmt.getTypeTable().getFullPath());
+        
+        // gets data represented as number of columns
+        for(Vector<String> row : asgmt.getStringTable()){
+            for(String cell: row){
+				System.out.print(cell + " ");
+			}
+			System.out.println(); //next line after a row
+        }
     }
 }
