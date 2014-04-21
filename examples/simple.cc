b4e37c1e8b51e98115f100814101527cd47c1e0d
Dmitry Romanov <romanov@jlab.org>
2014-04-21 17:58:48 +0000

    simple example update
    git-svn-id: https://phys12svn.jlab.org/repos/trunk/ccdb@1841 c5ed4466-e916-0410-8347-b3263e9c103d

--- examples/simple.cc
+++ examples/simple.cc
@@ -1,4 +1,9 @@
+/**
+ * 	Simpliest example of how to get access to CCDB data
+ */
+
 #include <CCDB/Calibration.h>
+#include <CCDB/CalibrationGenerator.h>
 #include <CCDB/SQLiteCalibration.h>
 #include <stdio.h>
 #include <string>
@@ -19,24 +24,25 @@ string create_connection_string()
 
 int main()
 {
-	//Create calibraion object
-	//auto_ptr<Calibration> calib((Calibration*)new SQLiteCalibration(/*run = */ 0, "default", 0));
-	SQLiteCalibration *calib = new SQLiteCalibration(100);
-    
-    //get some connection string
-    
-    string connection_str = create_connection_string();    
-    if (!connection_str.size())
+	//Obtain connection string. You can change it to "mysql://localhost for example
+	string connection_str = create_connection_string();
+	if (!connection_str.size())
     {
 		cout<<"No connection string"<<endl;
 		return 1;
 	}
 	cout<<"Connecting to "<<connection_str<<endl;
 	
-	//connect
-	int result = calib->Connect(connection_str);
-	cout<<"result is"<<result<<endl;
-	//get data
+	
+	// Create calibraion object
+	// To specify run number, variation and time, the full signature of CreateCalibration is:
+	// CreateCalibration(connection_str, int run=0, const string& variation="default", const time_t time=0);
+	//
+	// (!) remark: CreateCalibration throws logic_error with explanation if something goes wrong
+	auto_ptr<Calibration> calib(CalibrationGenerator::CreateCalibration(connection_str));
+	
+	// The calibration object is now connected and ready to work. Lets get data:
+	
 	vector<vector<double> > data;
 	calib->GetCalib(data, "/test/test_vars/test_table");
 		
@@ -49,4 +55,14 @@ int main()
 		}
 		cout<<endl;
 	}
+	
+	//The data could be
+	//vector<double> data;          =>  data[1] - row index 0, col index 1
+	//vector<vector<double> >       =>  data[0][1] - row index 0, col index 1
+	//vector<map<string, double> >  =>  data[0]["y"] - row index 0, "y" column
+	//map<string, double> >         =>  data["y"] - row index 0, "y" column	
+	//Type is int, string, double
+	//
+	//for more sophisticated data manipulation, like getting column types, 
+	//see assignments.cc example
 }
