6f42253360970a74604d84c80ba63e110631ddbe
Dmitry Romanov <romanov@jlab.org>
2015-12-03 14:28:57 -0500

    MySQL password is asked by --mysql-pwd flag instead of -p, resolving #3

--- python/ccdb/cmd/__init__.py
+++ python/ccdb/cmd/__init__.py
@@ -1,6 +1,6 @@
 from .console_context import ConsoleContext
 from .console_util import ConsoleUtilBase
 from .utility_argument_parser import UtilityArgumentParser, ArgumentParseError
-from colorama import Fore, Back, Style
+from .colorama import Fore, Back, Style
 
 
