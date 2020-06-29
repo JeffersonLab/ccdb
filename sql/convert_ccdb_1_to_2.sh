396a4087fabd42f9f72e644509628d8991ecc139
markito3 <marki@jlab.org>
2020-06-29 13:18:30 -0400

    script to convert ccdb 1 to ccdb 2

---
+++ sql/convert_ccdb_1_to_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+host=localhost
+scratch_dir=/u/scratch/$USER/ccdb_convert
+echo host is $host
+echo scratch_dir is $scratch_dir
+echo delete scratch_dir
+rm -rf $scratch_dir
+echo create scratch_dir
+mkdir -p $scratch_dir
+echo dump ccdb 1
+mysqldump -h$host -uccdb_user ccdb > $scratch_dir/ccdb1.sql
+echo drop ccdb 2 database
+mysql -h$host -uccdb_user -e "drop database if exists ccdb2"
+echo create ccdb 2 database
+mysql -h$host -uccdb_user -e "create database ccdb2"
+echo load ccdb 2 database from ccdb 1 dump
+mysql -h$host -uccdb_user ccdb2 < $scratch_dir/ccdb1.sql
+echo convert ccdb 2 to ccdb 2 schema
+mysql -h$host -uccdb_user ccdb2 < $CCDB_HOME/sql/update_1.00_2.00.sqlite.sql
