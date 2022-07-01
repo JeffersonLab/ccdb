513b2c73e3dfc433bc7f051818559e6af1e6773a
Dmitry Romanov <romanovda@gmail.com>
2022-07-01 01:54:57 -0400

    MySQL script update

--- sql/update_1.00_2.00.mysql.sql
+++ sql/update_1.00_2.00.mysql.sql
@@ -1,26 +1,26 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
-
 ALTER TABLE `variations`
-    ADD COLUMN `isLocked` TINYINT(1) NOT NULL DEFAULT 0 AFTER `parentId`,
-    ADD COLUMN `lockTime` TIMESTAMP NULL DEFAULT NULL AFTER `isLocked`,
-    ADD COLUMN `lockAuthorId` INT(11) NULL DEFAULT NULL AFTER `lockTime`,
-    ADD COLUMN `goBackBehavior` INT(11) NOT NULL DEFAULT 0 AFTER `lockAuthorId`,
-    ADD COLUMN `goBackTime` TIMESTAMP NULL DEFAULT NULL AFTER `goBackBehavior`;
+ADD COLUMN `isLocked` TINYINT(1) NOT NULL DEFAULT 0 AFTER `parentId`,
+ADD COLUMN `lockTime` TIMESTAMP NULL DEFAULT NULL AFTER `isLocked`,
+ADD COLUMN `lockedByUserId` INT(11) NULL DEFAULT NULL AFTER `lockTime`,
+ADD COLUMN `goBackBehavior` INT(11) NOT NULL DEFAULT 0 AFTER `lockedByUserId`,
+ADD COLUMN `goBackTime` TIMESTAMP NULL DEFAULT NULL AFTER `goBackBehavior`,
+ADD COLUMN `isDeprecated` TINYINT(1) NOT NULL DEFAULT 0 AFTER `goBackTime`,
+ADD COLUMN `deprecatedByUserId` INT(11) NULL DEFAULT NULL AFTER `isDeprecated`;
 
 
-ALTER TABLE `typeTables` 
-    ADD COLUMN `isDeprecated` TINYINT(1) NOT NULL DEFAULT 0 AFTER `comment`,
-    ADD COLUMN `deprecatedById` INT(11) NOT NULL DEFAULT -1 AFTER `isDeprecated`,
-    ADD COLUMN `isLocked` TINYINT(1) NOT NULL DEFAULT 0 AFTER `deprecatedById`,
-    ADD COLUMN `lockAuthorId` INT(11) NULL DEFAULT NULL AFTER `isLocked`,
-    ADD COLUMN `lockTime` TIMESTAMP NULL DEFAULT NULL AFTER `lockAuthorId`;
+ALTER TABLE `typeTables`
+ADD COLUMN `isDeprecated` TINYINT(1) NOT NULL DEFAULT 0 AFTER `comment`,
+ADD COLUMN `deprecatedByUserId` INT(11) NULL DEFAULT NULL AFTER `isDeprecated`,
+ADD COLUMN `isLocked` TINYINT(1) NOT NULL DEFAULT 0 AFTER `deprecatedByUserId`,
+ADD COLUMN `lockedByUserId` INT(11) NULL DEFAULT NULL AFTER `isLocked`,
+ADD COLUMN `lockTime` TIMESTAMP NULL DEFAULT NULL AFTER `lockedByUserId`;
+
 
 ALTER TABLE `directories`
-    ADD COLUMN `isDeprecated` TINYINT(1) NOT NULL DEFAULT 0 AFTER `comment`,
-    ADD COLUMN `deprecatedById` INT(11) NOT NULL DEFAULT -1 AFTER `isDeprecated`;
-
+ADD COLUMN `isDeprecated` TINYINT(1) NOT NULL DEFAULT 0 AFTER `comment`,
+ADD COLUMN `deprecatedByUserId` INT(11) NULL DEFAULT NULL AFTER `isDeprecated`,
+ADD COLUMN `isLocked` TINYINT(1) NOT NULL DEFAULT 0 AFTER `deprecatedByUserId`,
+ADD COLUMN `lockedByUserId` INT(11) NULL DEFAULT NULL AFTER `isLocked`;
 
 CREATE TABLE IF NOT EXISTS `assignmentsMaterializedView` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -32,43 +32,8 @@ CREATE TABLE IF NOT EXISTS `assignmentsMaterializedView` (
   `runMin` INT(11) NOT NULL,
   `runMax` INT(11) NOT NULL,
   `assignmentTime` TIMESTAMP NOT NULL,
-  INDEX `fk_assignmentsMaterializedView_assignments1_idx` (`assignmentsId` ASC),
-  INDEX `fk_assignmentsMaterializedView_variations1_idx` (`variationsId` ASC),
-  INDEX `fk_assignmentsMaterializedView_constantSets1_idx` (`constantSetsId` ASC),
-  INDEX `fk_assignmentsMaterializedView_typeTables1_idx` (`typeTablesId` ASC),
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_assignmentsMaterializedView_runRanges1_idx` (`runRangesId` ASC),
-  CONSTRAINT `fk_assignmentsMaterializedView_assignments1`
-    FOREIGN KEY (`assignmentsId`)
-    REFERENCES `assignments` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_assignmentsMaterializedView_variations1`
-    FOREIGN KEY (`variationsId`)
-    REFERENCES `variations` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_assignmentsMaterializedView_constantSets1`
-    FOREIGN KEY (`constantSetsId`)
-    REFERENCES `constantSets` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_assignmentsMaterializedView_typeTables1`
-    FOREIGN KEY (`typeTablesId`)
-    REFERENCES `typeTables` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_assignmentsMaterializedView_runRanges1`
-    FOREIGN KEY (`runRangesId`)
-    REFERENCES `runRanges` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  PRIMARY KEY (`id`));
+
 
 UPDATE `schemaVersions` SET schemaVersion = 5 WHERE `id` = 1;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
