8a38baa76c11a11e582bd38ddb4182f72997262e
Dmitry Romanov <romanovda@gmail.com>
2022-07-01 12:35:19 -0400

    MySQL Migration script

--- sql/update_1.00_2.00.mysql.sql
+++ sql/update_1.00_2.00.mysql.sql
@@ -1,39 +1,41 @@
 ALTER TABLE `variations`
-ADD COLUMN `isLocked` TINYINT(1) NOT NULL DEFAULT 0 AFTER `parentId`,
+ADD COLUMN `isLocked` TINYINT NOT NULL DEFAULT 0 AFTER `parentId`,
 ADD COLUMN `lockTime` TIMESTAMP NULL DEFAULT NULL AFTER `isLocked`,
-ADD COLUMN `lockedByUserId` INT(11) NULL DEFAULT NULL AFTER `lockTime`,
-ADD COLUMN `goBackBehavior` INT(11) NOT NULL DEFAULT 0 AFTER `lockedByUserId`,
+ADD COLUMN `lockedByUserId` INT NULL DEFAULT NULL AFTER `lockTime`,
+ADD COLUMN `goBackBehavior` INT NOT NULL DEFAULT 0 AFTER `lockedByUserId`,
 ADD COLUMN `goBackTime` TIMESTAMP NULL DEFAULT NULL AFTER `goBackBehavior`,
-ADD COLUMN `isDeprecated` TINYINT(1) NOT NULL DEFAULT 0 AFTER `goBackTime`,
-ADD COLUMN `deprecatedByUserId` INT(11) NULL DEFAULT NULL AFTER `isDeprecated`;
+ADD COLUMN `isDeprecated` TINYINT NOT NULL DEFAULT 0 AFTER `goBackTime`,
+ADD COLUMN `deprecatedByUserId` INT NULL DEFAULT NULL AFTER `isDeprecated`;
 
 
 ALTER TABLE `typeTables`
-ADD COLUMN `isDeprecated` TINYINT(1) NOT NULL DEFAULT 0 AFTER `comment`,
-ADD COLUMN `deprecatedByUserId` INT(11) NULL DEFAULT NULL AFTER `isDeprecated`,
-ADD COLUMN `isLocked` TINYINT(1) NOT NULL DEFAULT 0 AFTER `deprecatedByUserId`,
-ADD COLUMN `lockedByUserId` INT(11) NULL DEFAULT NULL AFTER `isLocked`,
+ADD COLUMN `isDeprecated` TINYINT NOT NULL DEFAULT 0 AFTER `comment`,
+ADD COLUMN `deprecatedByUserId` INT NULL DEFAULT NULL AFTER `isDeprecated`,
+ADD COLUMN `isLocked` TINYINT NOT NULL DEFAULT 0 AFTER `deprecatedByUserId`,
+ADD COLUMN `lockedByUserId` INT NULL DEFAULT NULL AFTER `isLocked`,
 ADD COLUMN `lockTime` TIMESTAMP NULL DEFAULT NULL AFTER `lockedByUserId`;
 
 
 ALTER TABLE `directories`
-ADD COLUMN `isDeprecated` TINYINT(1) NOT NULL DEFAULT 0 AFTER `comment`,
-ADD COLUMN `deprecatedByUserId` INT(11) NULL DEFAULT NULL AFTER `isDeprecated`,
-ADD COLUMN `isLocked` TINYINT(1) NOT NULL DEFAULT 0 AFTER `deprecatedByUserId`,
-ADD COLUMN `lockedByUserId` INT(11) NULL DEFAULT NULL AFTER `isLocked`;
+ADD COLUMN `isDeprecated` TINYINT NOT NULL DEFAULT 0 AFTER `comment`,
+ADD COLUMN `deprecatedByUserId` INT NULL DEFAULT NULL AFTER `isDeprecated`,
+ADD COLUMN `isLocked` TINYINT NOT NULL DEFAULT 0 AFTER `deprecatedByUserId`,
+ADD COLUMN `lockedByUserId` INT NULL DEFAULT NULL AFTER `isLocked`;
 
 CREATE TABLE IF NOT EXISTS `assignmentsMaterializedView` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `assignmentsId` INT(11) NOT NULL,
-  `variationsId` INT(11) NOT NULL,
-  `constantSetsId` INT(11) NOT NULL,
-  `typeTablesId` INT(11) NOT NULL,
-  `runRangesId` INT(11) NOT NULL,
-  `runMin` INT(11) NOT NULL,
-  `runMax` INT(11) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `assignmentsId` INT NOT NULL,
+  `variationsId` INT NOT NULL,
+  `constantSetsId` INT NOT NULL,
+  `typeTablesId` INT NOT NULL,
+  `runRangesId` INT NOT NULL,
+  `runMin` INT NOT NULL,
+  `runMax` INT NOT NULL,
   `assignmentTime` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`));
 
 
 UPDATE `schemaVersions` SET schemaVersion = 5 WHERE `id` = 1;
+
+
 
